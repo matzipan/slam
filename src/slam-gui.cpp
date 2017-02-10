1d70f84614f7f03a5b14b336dcbc289955fd2ccb
Andrei Zisu <matzipan@gmail.com>
2017-02-10 01:07:01 +0000

    Forgot to commit gui main file #7

---
+++ src/slam-gui.cpp
@@ -0,0 +1,26 @@
+//
+// Created by matzipan on 06/02/17.
+//
+
+#include <src/plotting/PlotController.h>
+#include "src/plotting/WindowPlot.h"
+
+int main(int argc, char *argv[]) {
+    QApplication application(argc, argv);
+
+    WindowPlot plot;
+    plot.show();
+    plot.setGeometry(10, 10, 1024, 768);
+    plot.plot();
+
+    PlotController controller(&plot);
+
+    controller.start();
+
+    int returnValue = application.exec();
+
+    controller.stop();
+    controller.wait();
+
+    return returnValue;
+}
